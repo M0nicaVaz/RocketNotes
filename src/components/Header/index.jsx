b2a3a78b6262b48e0e15eaf1aa16e2577384c64e
Monica Vaz <contatomonicavaz@gmail.com>
2022-07-22 18:31:37 -0300

    fix signout error

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,9 +5,17 @@ import { Container, Profile, Logout } from './styles';
 
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 import { api } from '../../services/api';
+import { useNavigate } from 'react-router-dom';
 
 export function Header() {
   const { signOut, user } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSignOut() {
+    signOut();
+
+    navigate('/');
+  }
 
   const avatarUrl = user.avatar
     ? `${api.defaults.baseURL}/files/${user.avatar}`
@@ -24,7 +32,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
